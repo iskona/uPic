0498620bc7aa39b4033c6a09164a4bfa268f186c
Ganesh Chittella <madhavi.0108@gmail.com>
2020-05-06 15:10:53 -0700

    added MERN folder structure

---
+++ client/src/components/MainContent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function MainContent() {
+    return (
+        <div className="jumbotron m-5  mx-auto bg-info">
+         <h1 class="display-4">Find your Inspiration !!</h1>
+         <h2>Join uPic Community to start a new Photo Contest or to get involved !!</h2>
+         <p className="lead">
+            <a className="btn btn-primary btn-lg" href="#" role="button">Start for Free</a>
+        </p>
+      </div>
+    )
+}
+
+export default MainContent
