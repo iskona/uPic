bf050d7d6285da8e0c1f7978533bd77d38423ad1
iskona <isony.d@gmail.com>
2020-05-18 21:29:16 -0700

    fixed contest list display bug

--- controllers/contestController.js
+++ controllers/contestController.js
@@ -3,7 +3,7 @@ const db = require("../models/");
 // Defining methods for the contestsController
 module.exports = {
     findAll: function (req, res) {
-        db.Contest
+        db.Contest.find({})
             .then(data => {
                 console.log(data);
                 res.json(data);
