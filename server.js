3029f019e8b25ce02b241fd6a25dbab8ddb61b4c
Ganesh Chittella <madhavi.0108@gmail.com>
2020-06-04 16:23:13 -0700

    port number

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose");
 var session = require("express-session");
